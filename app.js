cd95abb49600caf9c7384f00052cb53a057adcb2
JungHun98 <j7papa@naver.com>
2023-09-24 14:23:02 +0900

    chore: 서버 기본 설정
    1. sqlite3 설치, node 연결

---
+++ app.js
@@ -0,0 +1,16 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+})
+
+// close the database connection
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+});
