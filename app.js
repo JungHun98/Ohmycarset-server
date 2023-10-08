3637b77eec9d068ffdffae1ad784ace15cf14e1d
JungHun98 <j7papa@naver.com>
2023-10-08 20:28:35 +0900

    feat: 카테고리 별 기본 정보 api 구현 info/:category를 endpoint로 데이터베이스 정보를 응답

--- app.js
+++ app.js
@@ -1,25 +1,63 @@
 const sqlite3 = require('sqlite3').verbose();
-const schema = require('./table-query.js');
+const express = require('express');
+const cors = require('cors');
 
-let db = new sqlite3.Database('./db/main.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the chinook database.');
-    
-    db.exec(schema, function (err) {
-      if (err) {
-        console.error('Error applying schema:', err.message);
-      } else {
-        console.log('Schema applied successfully.');
+const app = express();
+const port = 3001;
+
+let db = new sqlite3.Database('./db/main.db');
+
+app.use(cors());
+
+function snakeToCamel(str) {
+  return str.replace(/_([a-z])/g, function (match, letter) {
+    return letter.toUpperCase();
+  });
+}
+
+function formatData(obj){
+  const formatbject = obj.map((row) => {
+    const cardData = {};
+    for (const key in row) {
+      if (Object.prototype.hasOwnProperty.call(row, key)) {
+        const camelKey = snakeToCamel(key);
+        cardData[camelKey] = row[key];
       }
-    });
-})
-
-// close the database connection
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
     }
-    console.log('Close the database connection.');
+    return cardData;
+  });
+
+  return formatbject;
+}
+
+
+app.get('/info/:category', (req, res) => {
+  const categoryMap = {
+    "powertrain" : "Powertrain",
+    "driving-system" : "Driving_System",
+    "bodytype" : "Bodytype",
+    "interior-color" : "Interior_Color",
+    "exterior-color" : "Exterior_Color",
+    "wheel" : "Wheel"
+  }
+  const category = req.params.category;
+  const query = `SELECT * FROM ${categoryMap[category]}`;
+
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    const data = formatData(rows);
+
+    res.json({
+      data: data,
+      message: 'success',
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
