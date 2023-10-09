77ebf227ad3dd807ae68e377264de017d61bf21b
JungHun98 <j7papa@naver.com>
2023-10-09 22:28:00 +0900

    feat: 자세히보기 API 구현

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const db = new sqlite3.Database('./db/main.db');
 
 app.use(cors());
 
+const categoryMap = {
+  "powertrain": "Powertrain",
+  "driving-system": "Driving_System",
+  "bodytype": "Bodytype",
+  "interior-color": "Interior_Color",
+  "exterior-color": "Exterior_Color",
+  "wheel": "Wheel",
+  "additional-option": "Additional_Option"
+}
+
 const snakeToCamel = (str) => {
   return str.replace(/_([a-z])/g, (match, letter) => {
     return letter.toUpperCase();
@@ -30,47 +40,74 @@ const formatData = (obj) => {
   return formatbject;
 }
 
-const searchDB = (resolve, reject, query, dbParams) => {
-  db.all(query, dbParams, (err, rows) => {
-    if (err) {
-      reject(err);
-      return;
-    }
-    resolve(rows);
-  });
-};
-
-const getSearchDBPromise = (category, queryParam) => {
-  const categoryMap = {
-    "powertrain": "Powertrain",
-    "driving-system": "Driving_System",
-    "bodytype": "Bodytype",
-    "interior-color": "Interior_Color",
-    "exterior-color": "Exterior_Color",
-    "wheel": "Wheel",
-    "additional-option": "Additional_Option"
-  }
-
-  const query = `SELECT * FROM ${categoryMap[category]}${queryParam ? ' WHERE category = ?' : ''}`;
-  console.log(query);
-  const dbParams = queryParam ? [queryParam] : [];
-
-  return new Promise((resolve, reject) => {
-      searchDB(resolve, reject, query, dbParams);
-  });
-}
-
 app.get('/info/:category', (req, res) => {
   const category = req.params.category;
   const queryParam = req.query.category;
+  const query = `SELECT * FROM ${categoryMap[category]}${queryParam ? ` WHERE category = "${queryParam}"` : ''}`;
 
-  const promise = getSearchDBPromise(category, queryParam);
-  promise.then((result) => {
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      res.status(500).json({
+        error: err.message, // 클라이언트에게 에러 응답 전송
+      });
+      return;
+    }
     res.json({
-      data: formatData(result),
+      data: formatData(rows),
+      message: "success"
+    })    
+  });
+});
+
+const sendAddtionalDetail = (id, resObject) => {
+  const query = `
+  SELECT title, description, image_src
+  FROM Additional_Detail
+  WHERE option_id = ${id}`;
+
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      resObject.status(500).json({
+        error: err.message, // 클라이언트에게 에러 응답 전송
+      });
+    }
+    resObject.json({
+      data: formatData(rows),
       message: "success"
     });
   });
+}
+
+const sendOptionDetail = (table, id, resObject) => {
+  const query = `
+  SELECT Detail.title, Detail.description, Detail.info
+  FROM ${table} AS T
+  INNER JOIN Detail ON T.detail_id = Detail.id
+  WHERE T.id = ${id}`;
+
+  db.get(query, (err, row) => {
+    if (err) {
+      resObject.status(500).json({
+        error: err.message, // 클라이언트에게 에러 응답 전송
+      });
+    }
+    resObject.json({
+      data: row,
+      message: "success"
+    });
+  });
+}
+
+app.get('/detail/:category/:id', (req, res) => {
+  const category = req.params.category;
+  const id = req.params.id;
+  const optionDataTable = categoryMap[category];
+  if(optionDataTable){
+    optionDataTable === "Additional_Option" ? sendAddtionalDetail(id, res) : sendOptionDetail(optionDataTable, id, res);
+  }
+  else{
+    res.status(500);
+  }
 });
 
 app.listen(port, () => {
